237c0f88d064d84e9473b38b0c59e97100c636db
guilmeister <ebg.edwardbguillermo@yahoo.com>
2020-01-22 01:44:23 +0000

    added strict slashes false, host and port

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,12 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.strict_slashes = False
+
 
 @app.route('/')
 def index():
     return "Hello HBNB!"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port="5000")
